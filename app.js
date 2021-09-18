814a9e1e41cd80e3b63fc1801d88ac3e0c74bb59
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-09-17 23:24:39 -0400

    basic web routing

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello!');
+// Imports
+//  generic
+const config = require('./private/config.json');
+const http = require('http');
+
+//  express
+const express = require('express');
+const app = express();
+
+const routes = require('./lib/routes.js');
+
+// Web Config
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.use('/', routes);
+
+
+// Web Listen
+app.listen(config.PORT, () => {
+    console.log(`Listening on port ${config.PORT}`);
+});
+
+
+// NEXT
