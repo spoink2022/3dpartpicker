fe52b6d023d454eb44ca26866e7ea1fd9d309dc6
leeminze214 <leeminz214@gmail.com>
2021-09-18 15:50:56 -0400

    front end dynamic tool cards

--- public/js/main.js
+++ public/js/main.js
@@ -11,25 +11,27 @@ $( document ).ready(function() {
         let price= parts[i]['price_20']
         let partImage = parts[i]['image']
         let card = `
-            <div class='productCard'>
+            <div class='productCard'>\n
 
-                <a href = "THELINKKKK"><img src='${partImage}' alt='Photo'></img></a>
-                <p class='price'>${price}</p>
-                <p class='name'>${partName}</p>
-                <div class='tags'>
+                <a href = "THELINKKKK"><img src='${partImage}' alt='Photo'></img></a>\n
+                <p class='price'>${price}</p>\n
+                <p class='name'>${partName}</p>\n
+                <div class='tags'>\n
                 `;
         for (let i = 0; i < tags.length; i++){
             if(tags[i]) {card += tags[i]}
         }
-        card+='\n</div></div>'
+        card+='</div>\n</div>\n'
         cardsinfo.push(card)
         //card is complete for one
     }
     let count = 0;
     for (let i = 0; i < cards.length; i++){
         for (let j = 0; j < 5; j++){
-            print(count, cardsinfo[count])
-            cards[i].appendChild = cardsinfo[count]
+            //console.log(count, cardsinfo[count])
+            console.log(cards[i])
+            
+            cards[i].innerHTML += cardsinfo[count];
             if(count ==parts.length-1){
                 count = 0;
             }else{
