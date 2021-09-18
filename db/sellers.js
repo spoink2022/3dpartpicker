886448d15e5043801ccc6d9101aab3a5eaf06188
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-09-18 13:30:47 -0400

    backend update

---
+++ db/sellers.js
@@ -0,0 +1,13 @@
+const config = require('./config.js');
+
+module.exports.getSeller = async function(id) {
+    let query = 'SELECT * FROM sellers WHERE id=$1 LIMIT 1';
+    let res = await config.pquery(query, [id]);
+    return res[0];
+}
+
+module.exports.getAllSellers = async function() {
+    let query = 'SELECT * FROM sellers ORDER BY id ASC';
+    let res = await config.pquery(query, []);
+    return res;
+}
