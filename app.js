0f9b8c5bbab780e84a3109a3d0ec9ab0be5d2423
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-09-18 15:17:10 -0400

    favicon

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const config = require('./private/config.json');
 const http = require('http');
 
 //  express
+const favicon = require('serve-favicon');
+
 const express = require('express');
 const app = express();
 
@@ -13,7 +15,7 @@ const routes = require('./lib/routes.js');
 // Web Config
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+app.use(favicon('public/images/favicon.ico'));
 
 app.use('/', routes);
 
