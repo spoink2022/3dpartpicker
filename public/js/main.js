205896c1e01327f72128040e2377cf6b6d5d186d
leeminze214 <leeminz214@gmail.com>
2021-09-18 15:24:29 -0400

    front end update

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,42 @@
 $( document ).ready(function() {
 
 
-    let cards = docuemnt.getElementsByClassName('products')
-        for (let i = 0; i < parts.length; i++) {
-            //access tags, price, image, name 
-            let name = parts[i][name]
-            let tags = [parts[i][tag],parts[i][tag_2]]
-            let price= parts[i][price_20]
-            let image = parts[i][image]
-            let card = `
-                <div class='productCard'>
+    let cards = document.getElementsByClassName('products')
+    console.log(cards)
+    let cardsinfo = []
+    for (let i = 0; i < parts.length; i++) {
+        //access tags, price, image, name 
+        let partName = parts[i]['name']
+        let tags = [parts[i]['tag'],parts[i]['tag_2']]
+        let price= parts[i]['price_20']
+        let partImage = parts[i]['image']
+        let card = `
+            <div class='productCard'>
 
-                    <a href = "THELINKKKK"><img src='${image}' alt='Photo'></img></a>
-                    <p class='price'>${price}</p>
-                    <p class='name'>${name}</p>
-                    <div class='tags'>
-                    `
-            for (let i = 0; i < tags.length; i++){
-                if(tags[i]) {card += tag[i]}
-            }
-            card+='</div>'
-            //card is complete for one
-
-    
+                <a href = "THELINKKKK"><img src='${partImage}' alt='Photo'></img></a>
+                <p class='price'>${price}</p>
+                <p class='name'>${partName}</p>
+                <div class='tags'>
+                `;
+        for (let i = 0; i < tags.length; i++){
+            if(tags[i]) {card += tags[i]}
+        }
+        card+='\n</div></div>'
+        cardsinfo.push(card)
+        //card is complete for one
     }
+    let count = 0;
+    for (let i = 0; i < cards.length; i++){
+        for (let j = 0; j < 5; j++){
+            print(count, cardsinfo[count])
+            cards[i].appendChild = cardsinfo[count]
+            if(count ==parts.length-1){
+                count = 0;
+            }else{
+                count++;
+            }
+        }
+    }
+    
+
 });
