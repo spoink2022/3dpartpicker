fd89a67317f1ddb587a5459869a4d8ab4e3d798c
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-09-18 18:32:56 -0400

    backend

--- db/accounts.js
+++ db/accounts.js
@@ -17,3 +17,10 @@ module.exports.getUser = async function(id) {
     let res = await config.pquery(query, [id]);
     return res[0];
 }
+
+module.exports.getNearbySellers = async function(lat, lon, maxDistanceKm) {
+    let maxDistanceDrg = maxDistanceKm/111;
+    let query = 'SELECT * FROM sellers WHERE SQRT(POWER(lat-$1, 2) + POWER(lon-$2, 2)) < $3 ORDER BY (POWER(lat-$1, 2) + POWER(lon-$2, 2)) LIMIT 10';
+    let res = await config.pquery(query, [lat, lon, maxDistanceDrg]);
+    return res;
+}
